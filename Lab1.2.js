2fbd01f874af393beaf854c60e25bdcdffddf94a
Володимир Крісанов <volodimirkrisanov@gmail.com>
2026-01-21 12:36:18 +0100

    Refactor increment functions and module exports
    Removed incObj function and updated exports.

--- Lab1.2.js
+++ Lab1.2.js
@@ -1,11 +1,12 @@
 // Друга реалізація: inc з сигнатурою inc(num: Num)
 // де Num є об'єктом з полем n, щоб функція змінила поле вихідного об'єкта
-function incObj(num) {
-  return ++num.n;
+const obj = { n: 5 };
+inc(obj);
+
+function inc(obj) {
+  obj.n = ++obj.n;
 }
 
-const obj = { n: 5 };
-incObj(obj);
 console.dir(obj);
 
-module.exports = { inc, incObj };
+module.exports = { inc };
